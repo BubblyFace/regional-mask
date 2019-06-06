0f1d5f314a4d130e670d05251d055ac0e04e37a6
BubblyFace <751835145@qq.com>
2019-06-06 10:09:27 +0800

    chore:: add transition

--- src/index.js
+++ src/index.js
@@ -13,31 +13,33 @@ function regionalMask(left, top, width, height, color, opacity) {
   const mask = document.getElementById('mask');
   const maskHeight = mask.offsetHeight;
   const maskWidth = mask.offsetWidth;
+
+  mask.addEventListener('touchstart', e => {
+    e.preventDefault();
+  }, false);
+
+  generateMaskBlock(0, 0, left, maskHeight, color, opacity, 'mask-left'); // left
+  generateMaskBlock(left + width, 0, maskWidth - left - width, maskHeight, color, opacity, 'mask-right'); // right
+  generateMaskBlock(left, 0, width, top, color, opacity, 'mask-top'); // top
+  generateMaskBlock(left, top + height, width, maskHeight - height - top, color, opacity, 'mask-bottom'); // bottom
+  generateMaskBlock(left, top, width, height, 'un-shadow'); // uns
+
+  function generateMaskBlock(left, top, width, height, color, opacity, id) {  
+    let dom = document.getElementById(id);
+
+    if(!dom) {
+      dom = document.createElement('div');
+      dom.id = id;
+      mask.appendChild(dom);
+    }
   
-  mask.innerHTML = ''
-
-  let maskBlocks = [
-    generateMaskBlock(0, 0, left, maskHeight, color, opacity, 'mask-left'), // left
-    generateMaskBlock(left + width, 0, maskWidth - left - width, maskHeight, color, opacity, 'mask-right'), // right
-    generateMaskBlock(left, 0, width, top, color, opacity, 'mask-top'), // top
-    generateMaskBlock(left, top + height, width, maskHeight - height - top, color, opacity, 'mask-bottom'), // bottom
-    generateMaskBlock(left, top, width, height, 'un-shadow') // uns
-  ]
-
-  maskBlocks.forEach(block => {
-    mask.appendChild(block);
-  })
+    dom.className = 'mask-block';
+    dom.style = `left:${left}rem;top:${top}rem;width:${width}rem;height:${height}rem;background:rgba(${color},${opacity})`;
+    
+  
+    return dom;
+  }
+  
 }
 
-function generateMaskBlock(left, top, width, height, color, opacity, id) {
-  let dom = document.createElement('div');
-
-  dom.className = 'mask-block';
-  dom.style = `left:${left}rem;top:${top}rem;width:${width}rem;height:${height}rem;background:rgba(${color},${opacity})`;
-  if(id) { 
-    dom.id = id;
-  } 
-
-  return dom;
-}
 
